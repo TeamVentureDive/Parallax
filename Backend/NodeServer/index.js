d03228183649682508ebbc024a680b8fb3f2479f
Philip-Bachl <bac2566@spengergasse.at>
2023-02-27 17:44:16 +0100

    file server basic (non-functional)

--- Backend/NodeServer/index.js
+++ Backend/NodeServer/index.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 const port = 1234;
 
-app.post("/upload", (req, res) =>{
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><form action="upload"><input type="file"></form></body></html>');
+});
+
+app.get("/upload", (req, res) =>{
     const form = formidable({multiples: true});
 
     form.parse(req, (err, fields, files) => {
