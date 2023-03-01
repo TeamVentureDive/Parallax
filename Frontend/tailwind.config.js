c6c1f17c2bbf6360c52a4f50ccd8b93a1ec9b3cb
SogehtTechnik <matteo.tripolt@vbc.at>
2023-03-01 16:46:26 +0100

    Improved Transitions

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,1 +1,13 @@
 //leere datei dient nur um die extension zu triggern für autocomplete
+extend: {
+
+    animation: {
+      shine: "shine 1s",
+    },
+    keyframes: {
+      shine: {
+        "100%": { left: "125%" },
+      },
+    },
+    
+  },
