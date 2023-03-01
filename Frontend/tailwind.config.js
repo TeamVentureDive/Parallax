f60c6f3a98aca0c38116a5c3155e61fc1eac47ae
SogehtTechnik <matteo.tripolt@vbc.at>
2023-03-01 16:51:35 +0100

    Corrected Error with Tailwind Config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,13 +1,1 @@
 //leere datei dient nur um die extension zu triggern für autocomplete
-extend: {
-
-    animation: {
-      shine: "shine 1s",
-    },
-    keyframes: {
-      shine: {
-        "100%": { left: "125%" },
-      },
-    },
-    
-  },
