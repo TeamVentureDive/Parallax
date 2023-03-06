7bd4972ef8558fcae7334607977b1f113106f723
AlexanderH <hac22567@spengergasse.at>
2023-03-06 17:22:55 +0100

    Add DB & POST after Conflicts in GIT

--- Backend/NodeServer/sendMail.js
+++ Backend/NodeServer/sendMail.js
@@ -2,6 +2,9 @@ var createCode = Math.floor(Math.random() * (9999999 - 1000000 + 1)) + 1000000;
 
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('para.db');
+const express = require('express');
+const app = express();
+var bodyParser  = require("body-parser");
 
 db.run(`INSERT INTO t_tempcode(t_code, t_active) VALUES(${createCode}, ${true})`, function(err) {
   if (err) {
@@ -25,27 +28,49 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-var mailOptions = {
-  from: 'parallax.venturedive.team@gmail.com',
-  to: `alex1907654@gmail.com`,
-  subject: 'Ihr Einmalcode',
-  text: `Hallo alex1907654@gmail.com,
 
-  wir haben Ihre Anforderung für einen Einmalcode für Ihr Parallax-Konto erhalten.
-  
-  Ihr Einmalcode lautet: ${createCode}
-  
-  Wenn Sie diesen Code nicht angefordert haben, können Sie diese E-Mail ignorieren, da Ihre E-Mail-Adresse u. U. versehentlich von einer anderen Person eingegeben wurde.
-  
-  Mit freundlichen Grüßen
-  Ihr Parallax-Konto-Team
-  `
-};
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/sendMail', (req, res) => {
+  const mail = req.body.email;
+  var mailOptions = {
+    from: 'parallax.venturedive.team@gmail.com',
+    to: `${mail}`,
+    subject: 'Ihr Einmalcode',
+    text: `Hallo ${mail},
+  
+    wir haben Ihre Anforderung für einen Einmalcode für Ihr Parallax-Konto erhalten.
+    
+    Ihr Einmalcode lautet: ${createCode}
+    
+    Wenn Sie diesen Code nicht angefordert haben, können Sie diese E-Mail ignorieren, da Ihre E-Mail-Adresse u. U. versehentlich von einer anderen Person eingegeben wurde.
+    
+    Mit freundlichen Grüßen
+    Ihr Parallax-Konto-Team
+    `
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+});
+
+
+const port = 6969;
+app.listen(port, () => {
+    console.log(`Database API started on Port: ${port}`);
 });
---
+++ Frontend/js/mail-address-send.js
@@ -0,0 +1,22 @@
+document.getElementById('login').addEventListener("click", () => {
+    let login = "http://localhost:6969/sendMail";
+    let emailInput = document.getElementById('email');
+    console.log(emailInput);
+    fetch(
+        login,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: emailInput.value
+        })})
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+    });
+    var s = window.location.href;
+    s = s.substring(0, s.indexOf("/reset"));
+    window.location.href = `${s}/resetPwToken.html`;
+});
