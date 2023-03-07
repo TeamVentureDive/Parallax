213320feb3ff3198b79d3e1cd3e7db7a9a463c34
LinusFue <91475295+LinusFue@users.noreply.github.com>
2023-03-07 18:50:02 +0100

    Delete upload.bat

--- upload.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p "message=Commit-Message: "
-set /p "branch=Commit-Branch: "
-git add .
-git checkout -b %branch%
-git commit -m "%message%"
-git push --set-upstream origin %branch%
-pause
-@echo on
