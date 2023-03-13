09c16f0494ca4ba2fe47a14e1754c1c1484f817d
LinusFue <91475295+LinusFue@users.noreply.github.com>
2023-03-13 17:04:36 +0100

    bitte sei da

---
+++ Backend/NodeServer/connectDb.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require("path");
+
+
+const db = new sqlite3.Database('../para.db', (error) => {
+    if (error) throw error;
+    console.log('Connected to SQLite database!');
+});
+
+
+module.exports = {db};
--- Backend/NodeServer/login/login.js
+++ Backend/NodeServer/login/login.js
@@ -1,16 +1,9 @@
-const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
-const path = require("path");
 const bodyParser = require('body-parser');
+const dbc = require('../connectDb.js');
 
 const app = express();
 
-
-
-const db = new sqlite3.Database('../para.db', (error) => {
-    if (error) throw error;
-    console.log('Connected to SQLite database!');
-});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,22 +13,15 @@ app.use((req, res, next) => {
     next();
 });
 
-// db.run('INSERT INTO a_accounts (a_username, a_email, a_password) VALUES ("testuser", "test@test.com", "password123")');
-// db.run('INSERT INTO f_files (f_a_email, f_name) VALUES ("test@test.com", "kekekekekekekek")');
-
-
-// app.get("/web", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../../../Frontend/login.html"));
-// });
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     if(isEmailValid(email)) {
-        db.get(`SELECT * FROM a_accounts WHERE a_email LIKE "` + email + '" AND a_password LIKE "' + password + '"', (err, row) => {
+        dbc.db.get(`SELECT * FROM a_accounts WHERE a_email LIKE "` + email + '" AND a_password LIKE "' + password + '"', (err, row) => {
             if(!row || err){
-            db.all('SELECT * FROM a_accounts inner join f_files on a_accounts.a_email = f_files.f_a_email WHERE a_email LIKE "' + email + '" AND a_password like "' + password + '"', (err, rows) => {
+                dbc.db.all('SELECT * FROM a_accounts inner join f_files on a_accounts.a_email = f_files.f_a_email WHERE a_email LIKE "' + email + '" AND a_password like "' + password + '"', (err, rows) => {
                 const json_data = JSON.stringify(rows);
                 if (err) res.json(email + 'User nicht in der Datenbank!');
                 res.json(email + 'User nicht in der Datenbank!');
@@ -53,11 +39,11 @@ app.post('/signup', (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
         if(isEmailValid(email)){
-        db.get('SELECT * FROM a_accounts WHERE a_email LIKE "' + email + '"', (err, row) => {
+            dbc.db.get('SELECT * FROM a_accounts WHERE a_email LIKE "' + email + '"', (err, row) => {
         if(row){
             res.status(409).json("User already exists");
         } else {
-            db.run(`INSERT INTO a_accounts (a_username, a_email, a_password) values("${username}", "${email}", "${password}")`, (err) => {
+            dbc.db.run(`INSERT INTO a_accounts (a_username, a_email, a_password) values("${username}", "${email}", "${password}")`, (err) => {
                 if(err){
                     res.json('Internal Server Error!' + row);
                 }else {
@@ -70,6 +56,7 @@ app.post('/signup', (req, res) => {
             res.json('"' + email + '" is not a valid Email!');
         }
 });
+
 
 const port = 6969;
 app.listen(port, "localhost", () => {
