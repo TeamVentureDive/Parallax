33a63633afa3e5baa92cfd5b009add98c1d3716f
LinusFue <91475295+LinusFue@users.noreply.github.com>
2023-03-13 17:07:39 +0100

    böser philip

--- Backend/NodeServer/connectDb.js
+++ Backend/NodeServer/connectDb.js
@@ -1,11 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
-const path = require("path");
-
 
 const db = new sqlite3.Database('../para.db', (error) => {
     if (error) throw error;
     console.log('Connected to SQLite database!');
 });
-
 
 module.exports = {db};
