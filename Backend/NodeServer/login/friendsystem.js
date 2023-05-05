648a963eb55615ee1d977e18cd1e73c6e2432b14
Philip-Bachl <bac2566@spengergasse.at>
2023-05-05 14:11:03 +0200

    continued to develop firend system + temp file

--- Backend/NodeServer/login/friendsystem.js
+++ Backend/NodeServer/login/friendsystem.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const fs = require("fs");
 const port = 6189;
 const dbc = require('../connectDb.js');
 
 app.get("/friend", (req, res) => {
-    console.log(req);
-    res.json({accept: false});
+    res.sendFile(path.join(__dirname, "temp.html"));
 }),
 
 app.listen(port);
