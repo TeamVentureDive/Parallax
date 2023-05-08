0bcbd7379393b89f467584ba03a1dd450bd30e0d
Philip-Bachl <bac2566@spengergasse.at>
2023-05-08 14:24:40 +0200

    "Added simplyfied and unified fileserver"

--- Backend/NodeServer/fileserver/download.js
+++ Backend/NodeServer/fileserver/download.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const app = express();
-const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-const fs = require("fs");
 
-app.get("/:fileId", (req, res) => {
-    const fileId = req.params.fileId;
-    db.get(`SELECT * FROM f_files WHERE f_id = "${fileId}"`, (err, row) => {
-        if (err) throw err;
-        res.sendFile(path.join(__dirname, "uploaded_files", fileId));
-        res.attachment(row.f_name);
+function start() {
+    app.get("/:fileId", (req, res) => {
+        const fileId = req.params.fileId;
+        db.get(`SELECT * FROM f_files WHERE f_id = "${fileId}"`, (err, row) => {
+            if (err) throw err;
+            res.sendFile(path.join(__dirname, "uploaded_files", fileId));
+            res.attachment(row.f_name);
+        });
     });
-});
+    
+    app.listen(420, () => {
+        console.log("[FileServer] Download Online");
+    });
+}
 
-app.listen(420, () => {
-    console.log("[FileServer] Download Online");
-});
-
-const db = new sqlite3.Database("../para.db", (err) => {
-    if (err) throw err;
-    console.log("[FileServer] Download Connected to SQLite Database");
-});
+module.exports = {start};
--- Backend/NodeServer/fileserver/upload.js
+++ Backend/NodeServer/fileserver/upload.js
@@ -3,106 +3,82 @@ const path = require("path");
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const db = require("../connectDb");
+const dbc = require("../connectDb");
 const urlHostname = "localhost"
 
-//DEBUG VVV
-app.get("/", (req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    
-    fs.readFile("temp.html", (err, data) => {
-        if (err) throw err;
-        res.send(data);
+function start() {
+    //DEBUG VVV
+    app.get("/", (req, res) => {
+        res.setHeader("Content-Type", "text/html");
+        
+        fs.readFile("temp.html", (err, data) => {
+            if (err) throw err;
+            res.send(data);
+        });
     });
-});
-//DEBUG ^^^
+    //DEBUG ^^^
 
-app.use(express.json());
+    app.use(express.json());
 
-app.post("/upload", async (req, res) => {
-    const form = new formidable.IncomingForm({uploadDir: path.join(__dirname, "uploaded_files")});
-    form.parse(req, async (err, fields, files) => {
-        res.status(400).setHeader("Content-Type", "application/json");
-        if (files.upload.length) {
-            for (let i = 0; i < files.upload.length; i++) {
-                uploadFile(res, fields, files.upload[i]);
-                res.send(JSON.stringify({uploadLink:  `http://${urlHostname}:420/${file.newFilename}`}));
+    app.post("/upload", async (req, res) => {
+        const form = new formidable.IncomingForm({uploadDir: path.join(__dirname, "uploaded_files")});
+        form.parse(req, async (err, fields, files) => {
+            res.status(400).setHeader("Content-Type", "application/json");
+            if (files.upload.length) {
+                for (let i = 0; i < files.upload.length; i++) {
+                    uploadFile(res, fields, files.upload[i]);
+                    res.send(JSON.stringify({uploadLink:  `http://${urlHostname}:420/${file.newFilename}`}));
+                }
+                return;
             }
-            return;
-        }
-        uploadFile(res, fields, files.upload); // New Implementation
-        res.send(JSON.stringify({uploadLink:  `http://${urlHostname}:420/${files.upload.newFilename}`}));
-        console.log(`[Fileserver-Upload] ${fields.email} uploaded file "${files.upload.originalFilename}" as "${files.upload.newFilename}"`);
+            uploadFile(res, fields, files.upload); // New Implementation
+            res.send(JSON.stringify({uploadLink:  `http://${urlHostname}:420/${files.upload.newFilename}`}));
+            console.log(`[Fileserver-Upload] ${fields.email} uploaded file "${files.upload.originalFilename}" as "${files.upload.newFilename}"`);
+        });
     });
-});
 
-app.listen(611, urlHostname);
+    app.listen(611, urlHostname);
 
-db.db.all(`SELECT * FROM f_files`, (err, rows) => {
-    rows.forEach(element => {
-        setTimeout(() => {
-            removeFromDatabase(element.f_id);
-            fs.unlinkSync(path.join(__dirname, "uploaded_files", element.f_id));
-        }, 3600000)
+    console.info("[FileServer] Upload Online")
+
+    dbc.db.all(`SELECT * FROM f_files`, (err, rows) => {
+        rows.forEach(element => {
+            setTimeout(() => {
+                removeFromDatabase(element.f_id);
+                fs.unlinkSync(path.join(__dirname, "uploaded_files", element.f_id));
+            }, 3600000)
+        });
     });
-});
 
-function uploadFile(res, fields, file) {
-    db.db.get(`SELECT * FROM a_accounts WHERE a_email LIKE "${fields.email}" AND a_password LIKE "${fields.password}"`, async (err, row) => {
-        if (err) throw err;
-        if (!row) {
-            fs.unlinkSync(path.join(__dirname, "uploaded_files", file.newFilename));
-            removeFromDatabase(file.newFilename);
-            res.status(401).json({upload:"blocked"});
-            console.log(`[FileServer-Upload] Blocked Upload of File "${file.originalFilename}" due to wrong credentials`);
-            return;
-        }
-        addToDatabase(file, fields.email);
-        setTimeout(() => {
-            fs.unlinkSync(path.join(__dirname, "uploaded_files", file.newFilename));
-            removeFromDatabase(file.newFilename);
-        }, 3600000);
-    });
-}
-
-//OLD CODE VVV
-function isValidAccount(email, password) {
-    let returnValue = false;
-    db.db.get(`SELECT * FROM a_accounts WHERE a_email LIKE "${email}" AND a_password LIKE "${password}"`, (err, row) => {
-
-    });
-}
-
-function isInDatabase(res, email, password, file) {
-    db.db.get(`SELECT * FROM a_accounts WHERE a_email LIKE "${email}" AND a_password LIKE "${password}"`, (err, row) => {
-
-        if (err) throw err;
-
-        if (row) {
-            addToDatabase(file, email);
+    function uploadFile(res, fields, file) {
+        dbc.db.get(`SELECT * FROM a_accounts WHERE a_email LIKE "${fields.email}" AND a_password LIKE "${fields.password}"`, async (err, row) => {
+            if (err) throw err;
+            if (!row) {
+                fs.unlinkSync(path.join(__dirname, "uploaded_files", file.newFilename));
+                removeFromDatabase(file.newFilename);
+                res.status(401).json({upload:"blocked"});
+                console.log(`[FileServer-Upload] Blocked Upload of File "${file.originalFilename}" due to wrong credentials`);
+                return;
+            }
+            addToDatabase(file, fields.email);
             setTimeout(() => {
                 fs.unlinkSync(path.join(__dirname, "uploaded_files", file.newFilename));
-                removeFromDatabase(file, file.newFilename);
+                removeFromDatabase(file.newFilename);
             }, 3600000);
+        });
+    }
 
-            return;
-        }
+    function addToDatabase(file, email) {
+        dbc.db.run(`INSERT INTO f_files values ("${file.newFilename}", "${file.originalFilename}", "${email}" ,"${new Date(Date.now()).toDateString()}")`, err => {
+            if (err) throw err;
+        });
+    }
 
-        fs.unlinkSync(path.join(__dirname, "uploaded_files", file.newFilename));
-        console.log(`[FileServer] Blocked Upload of File "${file.originalFilename}" due to wrong credentials`);
-        res.status(401).json(JSON.stringify({file: "blocked"}));
-    });
+    function removeFromDatabase(fileId) {
+        dbc.db.run(`DELETE FROM f_files WHERE f_a_email like "${fileId}"`, err => {
+            if (err) throw err;
+        });
+    }
 }
-//OLD CODE ^^^
 
-function addToDatabase(file, email) {
-    db.db.run(`INSERT INTO f_files values ("${file.newFilename}", "${file.originalFilename}", "${email}" ,"${new Date(Date.now()).toDateString()}")`, err => {
-        if (err) throw err;
-    });
-}
-
-function removeFromDatabase(fileId) {
-    db.db.run(`DELETE FROM f_files WHERE f_a_email like "${fileId}"`, err => {
-        if (err) throw err;
-    });
-}
+module.exports = {start};
