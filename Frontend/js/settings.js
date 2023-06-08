2b4ae75a175c45c8c9b7fe43887e19d2cc6344ce
SogehtTechnik <matteo.tripolt@vbc.at>
2023-06-08 16:30:36 +0200

    New Friendcode UI and Bugfixes

--- Frontend/js/settings.js
+++ Frontend/js/settings.js
@@ -4,3 +4,35 @@ function closeSettings() {
 function resetPassword() {
     window.location.href = "resetPwEmail.html";
 }
+
+
+
+// JavaScript code to handle button clicks and toggle sections
+const accountSettingsBtn = document.getElementById("accountSettingsBtn");
+const loginSecurityBtn = document.getElementById("loginSecurityBtn");
+const accountSettingsSection = document.getElementById("accountSettingsSection");
+const loginSecuritySection = document.getElementById("loginSecuritySection");
+
+accountSettingsBtn.addEventListener("click", function() {
+  accountSettingsBtn.classList.add("bg-gray-200");
+  loginSecurityBtn.classList.remove("bg-gray-200");
+  accountSettingsSection.classList.remove("hidden");
+  loginSecuritySection.classList.add("hidden");
+  accountSettingsBtn.classList.remove("font-light");
+  accountSettingsBtn.classList.add("font-semibold");
+  loginSecurityBtn.classList.add("font-light");
+  loginSecurityBtn.classList.remove("font-semibold");
+
+});
+
+loginSecurityBtn.addEventListener("click", function() {
+  accountSettingsBtn.classList.remove("bg-gray-200");
+  loginSecurityBtn.classList.add("bg-gray-200");
+  accountSettingsSection.classList.add("hidden");
+  loginSecuritySection.classList.remove("hidden");
+  accountSettingsBtn.classList.add("font-light");
+  accountSettingsBtn.classList.remove("font-semibold");
+  loginSecurityBtn.classList.remove("font-light");
+  loginSecurityBtn.classList.add("font-semibold");
+
+});
