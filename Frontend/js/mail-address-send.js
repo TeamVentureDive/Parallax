f021f16ecfd7cce18731926ec2b89779c25b9dc5
AlexanderH <hac22567@spengergasse.at>
2023-06-09 12:30:13 +0200

    Bachl fixt

--- Frontend/js/mail-address-send.js
+++ Frontend/js/mail-address-send.js
@@ -1,5 +1,5 @@
 document.getElementById('login').addEventListener("click", () => {
-    let sendTo = "http://localhost:80/sendMail";
+    let sendTo = "/sendMail";
     let emailInput = document.getElementById('email');
     sessionStorage.setItem("email", emailInput.value);
     fetch(
--- Frontend/js/mail-token-send.js
+++ Frontend/js/mail-token-send.js
@@ -1,5 +1,5 @@
 function checkDatabaseForCode(token) {
-     let sendTo = "http://localhost:80/checkVerification";
+     let sendTo = "/checkVerification";
      fetch(
         sendTo, {
             method: "POST",
